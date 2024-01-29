7667b65457b12d07c6ad256153191eb057187e16
Filip Žitný <filipzitny@gmail.com>
2024-01-29 21:15:03 +0100

    task 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+mod labyrinth_of_algorithms;
+
+fn main(){
+    let test_array = [1,2,3,4,5];
+    let result = labytinth_of_algorithm::maxTotal(&test_array);
+    println("The sun is:{}": result);
 }
